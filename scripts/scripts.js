ae15c16583ea555a4101c890573f09d75cf47d77
unknown <shamrajdebpuc1180@gmail.com>
2025-03-15 08:44:56 +0600

    added Display Date

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,12 @@ document.querySelector('.go-to-QNA').addEventListener('click', () => {
     window.location.href = 'QNA.html';
 });
 
+// // Display Date
+let today = new Date();
+
+  let day = today.toLocaleDateString('en-US', { weekday: 'short' });
+  let fullDate = today.toLocaleDateString('en-US', { month: 'short', day: '2-digit', year: 'numeric' });
+  document.querySelector('.day').textContent = day + ','; 
+  document.querySelector('.date').textContent = fullDate; 
+
+
